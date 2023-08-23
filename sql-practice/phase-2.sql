b85c5888ffcfbcdc273632dbeda2729c956cc486
Brian <brian_@DESKTOP-GJH2BCP>
2023-08-23 19:07:30 -0400

    first commit, phase2 finished

--- sql-practice/phase-2.sql
+++ sql-practice/phase-2.sql
@@ -1,1 +1,20 @@
--- Your code here
+DROP TABLE IF EXISTS customers;
+drop table if exists coffee_orders;
+
+Create Table customers (
+  id integer primary key autoincrement,
+  name varchar(40) NOT NULL,
+  phone numeric (10,0) UNIQUE,
+  email varchar(255) UNIQUE,
+  points integer DEFAULT 5,
+  created_at timestamp default current_time NOT NULL
+);
+
+CREATE Table coffee_orders (
+  id integer primary key autoincrement,
+    is_redeemed boolean default "FALSE",
+    ordered_at timestamp default current_time NOT NULL
+--   user id NOT NULL,
+--   foreign key (user_id) references users(id)
+
+);
